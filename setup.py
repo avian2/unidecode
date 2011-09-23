376a54fa19d071581fe4ced4521030ca2d7c93c9
Tomaz Solc <tomaz.solc@tablix.org>
2011-09-23 08:49:49 +0200

    Increased version to 0.04.9

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ class TestCommand(Command):
 		result = unittest.TextTestRunner(verbosity=2).run(suite)
 
 setup(name='Unidecode',
-      version='0.04.8',
+      version='0.04.9',
       description='ASCII transliterations of Unicode text',
       license='GPL',
       long_description=open("README").read(),
