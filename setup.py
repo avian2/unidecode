aaeb6b3ab658f9bcfdcdf5bac6cac0645ec423b2
Jan Killian <jan.killian@gmail.com>
2013-11-20 08:26:51 +0100

    updated version and changelog (0.4.8)

--- setup.py
+++ setup.py
@@ -4,29 +4,29 @@ from distutils.core import Command, setup
 import unittest
 
 UNITTESTS = [
-		"tests", 
-	]
+    "tests",
+  ]
 
 class TestCommand(Command):
-	user_options = [ ]
+  user_options = [ ]
 
-	def initialize_options(self):
-		pass
+  def initialize_options(self):
+    pass
 
-	def finalize_options(self):
-		pass
+  def finalize_options(self):
+    pass
 
-	def run(self):
-		suite = unittest.TestSuite()
+  def run(self):
+    suite = unittest.TestSuite()
 
-		suite.addTests( 
-			unittest.defaultTestLoader.loadTestsFromNames( 
-								UNITTESTS ) )
+    suite.addTests(
+      unittest.defaultTestLoader.loadTestsFromNames(
+                UNITTESTS ) )
 
-		result = unittest.TextTestRunner(verbosity=2).run(suite)
+    result = unittest.TextTestRunner(verbosity=2).run(suite)
 
 setup(name='Unidecode',
-      version='0.4.7',
+      version='0.4.8',
       description='US-ASCII transliterations of Unicode text',
       license='Perl',
       long_description="""
@@ -39,15 +39,15 @@ that's nearly useless to the user who actually wants to read what
 the text says.
 
 What Unidecode provides is a function, 'unidecode(...)' that
-takes Unicode data and tries to represent it in ASCII characters 
-(i.e., the universally displayable characters between 0x00 and 0x7F). 
-The representation is almost always an attempt at transliteration 
--- i.e., conveying, in Roman letters, the pronunciation expressed by 
+takes Unicode data and tries to represent it in ASCII characters
+(i.e., the universally displayable characters between 0x00 and 0x7F).
+The representation is almost always an attempt at transliteration
+-- i.e., conveying, in Roman letters, the pronunciation expressed by
 the text in some other writing system.
 
 For example 'unidecode(u"\u5317\u4EB0")' returns 'Bei Jing'.
 
-This is a Python port of Text::Unidecode Perl module by 
+This is a Python port of Text::Unidecode Perl module by
 Sean M. Burke <sburke@cpan.org>.
       """,
       author='Tomaz Solc',
