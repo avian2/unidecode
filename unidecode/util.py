701e7709809bf73f76c603ad121671473beb428b
Tomaz Solc <tomaz.solc@tablix.org>
2015-05-13 18:33:32 +0200

    Use optparse for Python 2.6 compatibility.

--- unidecode/util.py
+++ unidecode/util.py
@@ -1,6 +1,6 @@
 # vim:ts=4 sw=4 expandtab softtabstop=4
 from __future__ import print_function
-import argparse
+import optparse
 import locale
 import os
 import sys
@@ -11,25 +11,20 @@ from unidecode import unidecode
 PY3 = sys.version_info[0] >= 3
 
 def main():
-    parser = argparse.ArgumentParser('unidecode')
-    parser.add_argument('-e',
-                        '--encoding',
-                        nargs=1,
-                        metavar='ENCODING',
-                        help='Specify an encoding, overriding system default')
-    parser.add_argument('file',
-                        nargs='?',
-                        metavar='FILE',
-                        help='File to transliterate')
+    default_encoding = locale.getpreferredencoding()
 
-    args = parser.parse_args()
-    if args.encoding:
-        encoding = args.encoding[0]
-    else:
-        encoding = locale.getpreferredencoding()
+    parser = optparse.OptionParser('%prog [options] [FILE]',
+            description="Transliterate Unicode text into ASCII. FILE is path to file to transliterate. "
+            "If omitted, standard input is used.")
+    parser.add_option('-e', '--encoding', metavar='ENCODING', default=default_encoding,
+            help='Specify an encoding (default is %s)' % (default_encoding,))
 
-    if args.file:
-        with open(args.file, 'rb') as f:
+    options, args = parser.parse_args()
+
+    encoding = options.encoding
+
+    if args:
+        with open(args[0], 'rb') as f:
             stream = f.read()
     else:
         if PY3:
@@ -40,8 +35,7 @@ def main():
     try:
         stream = stream.decode(encoding)
     except UnicodeDecodeError as e:
-        msg = ('Unable to decode input: {}, start: {:d}, '
-            'end: {:d}').format(e.reason, e.start, e.end)
+        msg = 'Unable to decode input: %s, start: %d, end: %d' % (e.reason, e.start, e.end)
         print(msg, file=sys.stderr)
         sys.exit(1)
 
