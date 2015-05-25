9b6eb6f78ddadca36c3476c7d420d9291d3b6092
Tomaz Solc <tomaz.solc@tablix.org>
2015-05-25 13:35:21 +0200

    Use full path when loading README.rst

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python
 
+import os
 from setuptools import setup
+
+def get_long_description():
+	return open(os.path.join(os.path.dirname(__file__), "README.rst")).read()
 
 setup(name='Unidecode',
       version='0.04.17',
       description='ASCII transliterations of Unicode text',
       license='GPL',
-      long_description=open("README.rst").read(),
+      long_description=get_long_description(),
       author='Tomaz Solc',
       author_email='tomaz.solc@tablix.org',
 
