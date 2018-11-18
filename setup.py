e3c730e5830f15c613f65f1764258165f4e5f52f
Tomaz Solc <tomaz.solc@tablix.org>
2018-11-18 12:56:32 +0100

    Set version to 1.0.23

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def get_long_description():
 
 setup(
     name='Unidecode',
-    version='1.0.22',
+    version='1.0.23',
     description='ASCII transliterations of Unicode text',
     license='GPL',
     long_description=get_long_description(),
