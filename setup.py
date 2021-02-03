4a1bf091cc65207704dbb6269603f0d174d229c7
Pascal Corpet <pascal@bayesimpact.org>
2021-02-03 14:05:45 +0100

    Add Typing stubs for the main API.
    See PEP 484 (for typing) and PEP 561 (for distributing types).

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
     author_email='tomaz.solc@tablix.org',
 
     packages=['unidecode'],
+    package_data={'unidecode': ['py.typed', '__init__.pyi']},
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
 
     test_suite='tests',
