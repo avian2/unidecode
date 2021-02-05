d173521bfa5291f3dcb1a0a71fc35eda6dae7840
Tomaz Solc <tomaz.solc@tablix.org>
2021-02-05 12:06:58 +0100

    Set version to 1.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def get_long_description():
 
 setup(
     name='Unidecode',
-    version='1.1.2',
+    version='1.2.0',
     description='ASCII transliterations of Unicode text',
     license='GPL',
     long_description=get_long_description(),
