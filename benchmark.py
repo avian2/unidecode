2a945f1d5394414393024f1f94242d1c150f9a2f
Marco Ribeiro <speaktomarco@gmail.com>
2022-08-11 19:37:00 -0300

    Fix print calls for Python 3

--- benchmark.py
+++ benchmark.py
@@ -1,26 +1,27 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 import timeit
 
 def main():
-	print "unidecode_expect_ascii, ASCII string"
+	print("unidecode_expect_ascii, ASCII string")
 	timeit.main([
 		'-s',
 		'from unidecode import unidecode_expect_ascii',
 		'unidecode_expect_ascii(u"Hello, World")'])
 
-	print "unidecode_expect_ascii, non-ASCII string"
+	print("unidecode_expect_ascii, non-ASCII string")
 	timeit.main([
 		'-s',
 		'from unidecode import unidecode_expect_ascii',
 		'unidecode_expect_ascii(u"¡Hola mundo!")'])
 
-	print "unidecode_expect_nonascii, ASCII string"
+	print("unidecode_expect_nonascii, ASCII string")
 	timeit.main([
 		'-s',
 		'from unidecode import unidecode_expect_nonascii',
 		'unidecode_expect_nonascii(u"Hello, World")'])
 
-	print "unidecode_expect_nonascii, non-ASCII string"
+	print("unidecode_expect_nonascii, non-ASCII string")
 	timeit.main([
 		'-s',
 		'from unidecode import unidecode_expect_nonascii',
